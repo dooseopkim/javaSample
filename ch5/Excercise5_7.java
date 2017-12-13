5a50dda2d5520998e730b394b088c81727c111a7
김두섭 <entjqcjswo@gmail.com>
2017-12-14 01:24:04 +0900

    1 자바의 정석 5장 연습문제

---
+++ ch5/Excercise5_7.java
@@ -0,0 +1,41 @@
+
+public class Excercise5_7 {
+
+	public static void main(String[] args) {
+		if(args.length!=1){
+			System.out.println("USAGE: java Excercies5_7 3120");
+			System.exit(0);
+		}
+		int money = Integer.parseInt(args[0]);
+		
+		System.out.println("money = " + money);
+		int[] coinUnit = {500,100,50,10};
+		int[] coin = {5,5,5,5};
+		
+		for(int i=0; i<coinUnit.length; i++){
+			int coinNum = 0;
+			coinNum = money/coinUnit[i];
+			if(coin[i]<coinNum){
+				coinNum = 5;
+				coin[i]-=coinNum;
+				money -= coinUnit[i]*coinNum;
+			} else {
+				coin[i]-=coinNum;
+				money = money%coinUnit[i];
+			}
+			
+			System.out.println(coinUnit[i] + "원: "+coinNum);
+
+		}
+		if(money>0){
+			System.out.println("거스름돈이 부족합니다.");
+			System.exit(0);
+		}
+		System.out.println("=남은 동전의 개수=");
+		
+		for(int i=0; i<coinUnit.length; i++){
+			System.out.println(coinUnit[i] + "원: "+coin[i]);
+		}
+	}
+
+}
